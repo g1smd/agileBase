2172b3f7a6a0e8a2be65d9ed0eccd7a36002ce82
Oliver Kohll <oliver@gtwm.co.uk>
2009-08-03 16:55:43 +0100

    Tweaks to ghost record style and behaviour

--- gtpb_clientside/deploy.sh
+++ gtpb_clientside/deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 cvs update -d
 ant -f deploy_templates_redhat.xml deploy
+echo "Compressing JavaScript and CSS files"
+find /usr/local/tomcat/apache-tomcat/webapps/portalBase/resources/ -name '*.js' -print -exec /usr/local/java/jdk1.6.0_02/bin/java -jar /usr/local/portalBase/yui_compressor/yuicompressor.jar -o {} {} \;
+find /usr/local/tomcat/apache-tomcat/webapps/portalBase/resources/ -name '*.css' -print -exec /usr/local/java/jdk1.6.0_02/bin/java -jar /usr/local/portalBase/yui_compressor/yuicompressor.jar -o {} {} \;
+find /usr/local/tomcat/apache-tomcat/webapps/portalBase/styles/ -name '*.css' -print -exec /usr/local/java/jdk1.6.0_02/bin/java -jar /usr/local/portalBase/yui_compressor/yuicompressor.jar -o {} {} \;
--- gtpb_server/deploy.sh
+++ gtpb_server/deploy.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 cvs update -d
 ant -f build_redhat.xml deploy
-find /usr/local/tomcat/apache-tomcat/webapps/portalBase/resources/ -name '*.js' -print -exec java -jar /usr/local/portalBase/yui_compressor/yuicompressor.jar -o {} {} \;
-find /usr/local/tomcat/apache-tomcat/webapps/portalBase/resources/ -name '*.css' -print -exec java -jar /usr/local/portalBase/yui_compressor/yuicompressor.jar -o {} {} \;
-find /usr/local/tomcat/apache-tomcat/webapps/portalBase/styles/ -name '*.css' -print -exec java -jar /usr/local/portalBase/yui_compressor/yuicompressor.jar -o {} {} \;
