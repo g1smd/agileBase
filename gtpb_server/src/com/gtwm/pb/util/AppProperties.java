ccf38d04201a7e95158cdb69d57f109eadb1ce73
Oliver Kohll <oliver@gtwm.co.uk>
2009-08-17 17:08:05 +0100

    Version 2.2.9 for release

--- gtpb_server/src/com/gtwm/pb/util/AppProperties.java
+++ gtpb_server/src/com/gtwm/pb/util/AppProperties.java
@@ -23,7 +23,7 @@ package com.gtwm.pb.util;
 public class AppProperties {
 	public static final String applicationName = "GT portalBase";
 
-	public static final String applicationVersion = "2.2.8";
+	public static final String applicationVersion = "2.2.9";
 
 	/**
 	 * Location of the Velocity template used to report template processing
