8d02ae77a94f1d046432a728991b940727dd8813
Oliver Kohll <oliver@gtwm.co.uk>
2010-01-29 11:19:34 +0000

    Remove summary debugging

--- gtpb_server/scripts/upgrades/update_for_2_5_portalbaseschema.sql
+++
@@ -1,5 +0,0 @@
-update reportsummarydefn set report_internalreportname=match.internalreportname
-from
-(select brd.internalreportname, rsd.id
-from basereportdefn brd inner join reportsummarydefn rsd on brd.reportsummary_id = rsd.id) as match
-where match.id = reportsummarydefn.id;
