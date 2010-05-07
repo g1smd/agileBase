26fc67d6616714773d5cdb3d0375d545042df20c
Oliver Kohll <oliver@gtwm.co.uk>
2010-05-07 16:26:25 +0100

    Aim to further improve load times by amalgamating a mess of JavaScript and CSS files into fewer files

--- gtpb_clientside/resources/tabs/tabs.js
+++ gtpb_clientside/resources/tabs/tabs.js
@@ -211,3 +211,234 @@ $(document).ready(function(){
 	pane3Scripts.functionList.push(fInit);
 	pane3Scripts.update();
 })
+
+/* 
+ * =========================================================
+ * End of tab engine functions.
+ * Start of general functions for to be used by the tabs themselves.
+ * First, the edit tab
+ */
+
+function fUnlockButton() {
+
+	var jqUnlockButton = $("#recordUnlockButton");
+	var jqUnlockDiv = $("#recordUnlock");
+	var jqBlankDiv = $("#recordBlank");
+
+	function fUnlockRecord() {
+		function fUnlockResult(sText, sXML) {
+			try {
+				if(sXML.getElementsByTagName('response')[0].firstChild.nodeValue!='ok') throw 'result_not_ok';
+				jqUnlockDiv.remove()
+				jqBlankDiv.remove();
+			}
+			catch(e) {
+				jqUnlockButton.removeAttr('disabled');
+				jqUnlockButton.find("div").empty().text("unlock record");
+				alert('the record could not be unlocked\nplease try again\n('+e+')');
+			}
+		}
+
+		jqUnlockButton.attr('disabled','true');
+		jqUnlockButton.find("div").empty().text("unlocking...");
+
+		var aPostVars=new Array();
+		aPostVars['return']='gui/administration/xmlreturn_fieldunlockrequest';
+		aPostVars['set_lock_override']='true';	    
+		aPostVars['returntype']='xml';
+		var oReq=new fRequest('AppController.servlet',aPostVars,fUnlockResult,-1);	        
+	}	
+
+	if (jqUnlockButton.length >= 1) {
+		jqUnlockButton.bind("click", fUnlockRecord);
+	}
+}
+
+
+function fComboComponents() {
+	// ComboComponent object
+	function fComboComponent(elem) {
+		var jqSelect = $(elem);
+		var globalEdit = false;
+		
+		// Extract unique field name and id end-string
+		var internalFieldName = jqSelect.attr("id");
+		if (internalFieldName.indexOf("global-edit") != -1) {
+			var globalEdit = true;			
+		}
+		
+		var idEndString = internalFieldName.substring(internalFieldName.indexOf("_dropdown")+9);
+		internalFieldName = internalFieldName.substring(0, internalFieldName.indexOf("_dropdown"));
+		
+		// Get associated text box
+		var jqTextBox = $("#"+internalFieldName+"_text"+idEndString);
+		jqTextBox.hide();
+		
+		// Either show the text box, or populate and update
+		function fDropdownChange(evt) {
+			if (jqSelect.val() == "new-custom-option") {
+				jqTextBox.show();
+				jqTextBox.val("");
+			} else {
+				jqTextBox.val(jqSelect.val())
+				jqTextBox.hide();
+				if (!globalEdit) {
+					new fChange(jqTextBox[0]);
+				}
+			}
+		}
+		
+		jqSelect.bind("change", fDropdownChange);
+	}
+
+	// Setup all combo-components
+	$('.combo-component').each(function(i) {
+		fComboComponent(this);
+	});
+}
+
+function fRelationPickers() {
+  function bindAutoComplete(jqElement, internalTableName, internalFieldName) {
+	      jqElement.autocomplete(
+				    "AppController.servlet", 
+					{
+					  autoFill: true,
+					  cacheLength: 100,
+					  max: 99,
+					  extraParams:{
+						    gtpb_return: "gui/resources/input/return_relation_values",
+						  	set_custom_field: true,
+						  	fieldkey: "relationField",
+						  	custominternaltablename: internalTableName,
+						  	custominternalfieldname: internalFieldName
+						  },
+					  mustMatch: true,
+					  selectFirst: true,
+					  width: 296
+				    }
+		   );
+		  
+		  jqElement.result(function(event, data, formatted) {
+			  if(data) {
+				  jqElement[0].formEl.doUpdate(data[1], true);
+			  } else {
+				  alert('Error saving data');
+			  }
+		  });
+  }
+    
+  $('.autocomplete').each(function(i) {
+	  if($.browser.msie) {
+		  //$('<span style="padding-right:10px">'+this.value+'</span>').insertBefore($(this));
+		  //$(this).remove();
+		  return;
+	  }
+	  else {
+		  var jqThis = $(this);
+		  var internalTableName = jqThis.attr('internalTableName');
+		  var internalFieldName = jqThis.attr('internalFieldName');
+		  bindAutoComplete(jqThis, internalTableName, internalFieldName);
+	  }
+  });
+				    
+}
+
+function appendWarningAction() {
+  var jqTableBody = $("#reportData > tbody");
+  if (jqTableBody.length > 0) {
+    // if message not already there
+    if (jqTableBody.find(".warningmessage").length == 0) {
+	  jqTableBody.append(warningRowHtmlSaved);
+    }
+  } else {
+	// TODO: possibility of infinite heap building up if reportData > tbody is never there, deal with this
+    setTimeout('appendWarningAction', 1000);
+  }
+}
+
+/* Called if the current record isn't visible in pane 2, to add a warning to that effect in the edit tab */
+function appendWarning(warningRowHtml) {
+  warningRowHtmlSaved = warningRowHtml;
+  setTimeout('appendWarningAction()', 1000);
+}
+
+var warningRowHtmlSaved = '';
+
+/* 
+ * Management tabs functions 
+ */
+
+function fShowTableUsage() {
+  // If no usage log table exists yet, get from server
+  if ($("#table_usage_loader table").size() == 0) {
+	  $.get("AppController.servlet",
+	  {"return":"gui/administration/tables/option_sets/table_usage_data_loader"},
+	  function(returned_content) {
+	    $("#table_usage_loader").html(returned_content);
+		$("#table_usage_loader").removeClass("load_spinner");
+	  });
+  }
+}
+
+function fShowReportUsage() {
+  // If no usage log table exists yet, get from server
+  if ($("#report_usage_loader table").size() == 0) {
+	  $.get("AppController.servlet",
+	  {"return":"gui/administration/reports/option_sets/report_usage_data_loader"},
+	  function(returned_content) {
+	    $("#report_usage_loader").html(returned_content);
+		$("#report_usage_loader").removeClass("load_spinner");
+	  });
+  }
+}
+
+/* Calculation editor */
+function fEnableCalcSyntaxHighlight() {
+  if($("#calculationdefn").length > 0) {
+    editAreaLoader.init({
+	  id: "calculationdefn"		// textarea id
+	  ,syntax: "sql"			    // syntax to be uses for highlighting
+	  ,start_highlight: true		// to display with highlight mode on start-up
+	  ,allow_toggle: false
+	  ,browsers: "all"
+	  ,toolbar: ""
+	  ,replace_tab_by_spaces: 2
+	  ,EA_load_callback: "fEALoaded"
+    });
+  }
+}
+
+function fEALoaded() {
+  $('#frame_calculationdefn').contents().find('.area_toolbar').hide();
+}
+
+/*
+ * Summary tab functions
+ */
+
+/* Charts in pane 3 need some behaviours added */
+function fSetupCharts() {
+  $('.summary_chart').each(function(i) {
+	  var summaryDivName = $(this).attr('id');
+	  var summaryId = summaryDivName.replace('chart_','');
+	  $(this).append("<div class='chart_remover'><a href='?return=gui/reports_and_tables/pane3&remove_report_summary=true&summaryid=" + summaryId + "'><img border='0' src='resources/icons/cross-greyscale-round.png' /></a></div>");
+	  $(this).hover(
+		function() {
+		  $(this).find('.chart_remover').fadeIn("normal");
+	    }, 
+	    function() {
+	      $(this).find('.chart_remover').fadeOut("normal");
+	    }
+	  );
+  });
+}
+
+/* ---------- Add functions to the callFunctions list ---------- */
+/* ------ These will be called every time a tab refreshes ------ */
+
+pane3Scripts.functionList.push(fUnlockButton);
+pane3Scripts.functionList.push(fComboComponents);
+pane3Scripts.functionList.push(fRelationPickers);
+pane3Scripts.functionList.push(fSetupCharts);
+
+
--- gtpb_clientside/resources/tabs/view.js
+++ gtpb_clientside/resources/tabs/view.js
@@ -15,42 +15,58 @@
  *  You should have received a copy of the GNU General Public License
  *  along with agileBase.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+/* 
+ * JS for view tab is separated out because the mobile version of agileBase
+ * only needs these functions, not those for the edit, manage etc. tabs
+ */
 function loadMap(postcode) {
-	if (!postcode) {return;}
-      if (GBrowserIsCompatible()) {
-        var geocoder = new GClientGeocoder();
-        geocoder.getLatLng(
-        	    postcode,
-        	    function(point) {
-        	      if (!point) {
-        	        alert("Postcode " + postcode + " not found");
-        	      } else {
-        	        var mapUrl = 'http://maps.google.com/staticmap?center='+point.toUrlValue()+'&zoom=11&size=100x100&maptype=terrain&key=ABQIAAAAAmhDcBizb6sHKLYdSFLnLBSsFD5D7A41QFa4vWfOgDnykADPDxRmS3oyj7HLtk0xVDNhc4xnV0s6sg&sensor=false';
-        	        $('#map').html('<a href="http://maps.google.co.uk/maps?f=q&hl=en&q='+postcode+'" target="_blank"><img border="1" src="'+mapUrl+'" /></a>');
-        	      }
-        	    }
-        	  );
-      }
+	if (!postcode) {
+		return;
+	}
+	if (GBrowserIsCompatible()) {
+		var geocoder = new GClientGeocoder();
+		geocoder
+				.getLatLng(
+						postcode,
+						function(point) {
+							if (!point) {
+								alert("Postcode " + postcode + " not found");
+							} else {
+								var mapUrl = 'http://maps.google.com/staticmap?center=' + point
+										.toUrlValue() + '&zoom=11&size=100x100&maptype=terrain&key=ABQIAAAAAmhDcBizb6sHKLYdSFLnLBSsFD5D7A41QFa4vWfOgDnykADPDxRmS3oyj7HLtk0xVDNhc4xnV0s6sg&sensor=false';
+								$('#map')
+										.html(
+												'<a href="http://maps.google.co.uk/maps?f=q&hl=en&q='
+														+ postcode
+														+ '" target="_blank"><img border="1" src="'
+														+ mapUrl + '" /></a>');
+							}
+						});
+	}
 }
 
 function fYouTube() {
-	$('a.gtpb_url').each(function(){
-		var sHref=this.getAttribute('href');
-		if((sHref.indexOf('youtube.com') > -1) || (sHref.indexOf('vimeo.com') > -1)) {
-			var oContainer=document.createElement('div');
-			oContainer=$(oContainer);
-			oContainer.addClass('gtpb_youtube');
-			$(this).replaceWith(oContainer);
-			oContainer.oembed(sHref);
-		}
-		//oContainer.find('embed').attr('rel',0);
-	});
+	$('a.gtpb_url').each(
+			function() {
+				var sHref = this.getAttribute('href');
+				if ((sHref.indexOf('youtube.com') > -1)
+						|| (sHref.indexOf('vimeo.com') > -1)) {
+					var oContainer = document.createElement('div');
+					oContainer = $(oContainer);
+					oContainer.addClass('gtpb_youtube');
+					$(this).replaceWith(oContainer);
+					oContainer.oembed(sHref);
+				}
+				// oContainer.find('embed').attr('rel',0);
+			});
 }
 
 /* ---------- Add functions to the callFunctions list ---------- */
 /* ------ These will be called every time a tab refreshes ------ */
 
-// Don't need to call loadMap when a tab loads, just need to have the fn available
+// Don't need to call loadMap when a tab loads, just need to have the fn
+// available
 // for when a map does need to be loaded
-//pane3Scripts.functionList.push(loadMap);
+// pane3Scripts.functionList.push(loadMap);
 pane3Scripts.functionList.push(fYouTube);
