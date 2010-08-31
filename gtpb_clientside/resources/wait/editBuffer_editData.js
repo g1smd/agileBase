cf5f2761e9e208aa4004514643e81505d247013f
Oliver Kohll <oliver@gtwm.co.uk>
2010-08-31 09:53:41 +0100

    A3 IE debugging

--- gtpb_clientside/resources/wait/editBuffer_editData.js
+++ gtpb_clientside/resources/wait/editBuffer_editData.js
@@ -554,7 +554,7 @@ function fChange(oObj)  {
     fClearWarning();
     var warningElement = sResponseXML.getElementsByTagName('warning')[0];
     if (typeof warningElement != "undefined") {
-    	alert(sResponseXML);
+    	alert(sResponseXML.xml);
       var sWarning = warningElement.firstChild.nodeValue;
       fSetWarning(sWarning);
     }
