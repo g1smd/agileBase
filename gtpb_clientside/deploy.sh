fde206bb99ce7d1f12e99178519a9adf9cd60f88
Oliver Kohll <oliver@gtwm.co.uk>
2010-10-29 21:27:33 +0100

    Module actions in pane 2 on iPad

--- gtpb_clientside/deploy.sh
+++ gtpb_clientside/deploy.sh
@@ -15,7 +15,7 @@ cat /usr/local/tomcat/apache-tomcat/webapps/agileBase/styles/report.css /usr/loc
 # pane 1
 cat /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/tree/tree.js /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/mobile/module_actions.js > /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/mobile/pane1_amalgam.js
 # pane 2
-cat /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/tables/new_delete.js /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/wait/request_setFilter.js /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/wait/jquery.ajaxmanager.js > /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/mobile/pane2_amalgam.js
+cat /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/tables/new_delete.js /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/wait/request_setFilter.js /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/wait/jquery.ajaxmanager.js /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/mobile/module_actions.js > /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/mobile/pane2_amalgam.js
 # pane 3
 cat /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/jquery.oembed.js /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/wait/editBuffer_editData.js /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/wait/request_setFilter.js /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/tabs/tabs.js /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/jquery.autocomplete.js /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/tabs/view.js > /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/mobile/pane3_amalgam.js
 # Compress JS and CSS
