04109426d8d558cf99580eb2ef8a4c45fc01f8e0
Oliver Kohll <oliver@gtwm.co.uk>
2010-10-29 12:55:03 +0100

    iPad module test in new page

--- gtpb_clientside/deploy_test.sh
+++ gtpb_clientside/deploy_test.sh
@@ -6,14 +6,14 @@ ant -f deploy_templates_ubuntu.xml deploy
 # Amalgamate JS and CSS
 # CSS
 #pane 1
-cat /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/tree/tree.css /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/module-colours.css /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/modalFramework/modalFramework.css /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/mobile/pane1_override.css > /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/mobile/pane1_amalgam.css
+cat /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/tree/tree.css /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/module-colours.css /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/mobile/pane1_override.css > /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/mobile/pane1_amalgam.css
 #pane 2
 cat /usr/local/tomcat/apache-tomcat/webapps/agileBase/styles/report.css /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/module-colours.css /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/mobile/override.css > /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/mobile/pane2_amalgam.css
 #pane 3
 cat /usr/local/tomcat/apache-tomcat/webapps/agileBase/styles/report.css /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/tabs/tabs.css /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/jquery.autocomplete.css /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/mobile/override.css > /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/mobile/pane3_amalgam.css
 # JS
 # pane 1
-cat /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/tree/tree.js /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/modalFramework/modalFramework.js /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/wait/request_setFilter.js > /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/mobile/pane1_amalgam.js
+cat /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/tree/tree.js /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/mobile/module_actions.js > /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/mobile/pane1_amalgam.js
 # pane 2
 cat /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/tables/new_delete.js /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/wait/request_setFilter.js /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/wait/jquery.ajaxmanager.js > /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/mobile/pane2_amalgam.js
 # pane 3
