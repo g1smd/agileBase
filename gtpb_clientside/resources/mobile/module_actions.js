c51e8b9ea2162ff6e3ba226ab1510376a8a6c006
Oliver Kohll <oliver@gtwm.co.uk>
2010-10-29 13:26:38 +0100

    iPad module test in new page

--- gtpb_clientside/resources/mobile/module_actions.js
+++ gtpb_clientside/resources/mobile/module_actions.js
@@ -1,8 +1,25 @@
+/*
+ *  Copyright 2010 GT webMarque Ltd
+ * 
+ *  This file is part of agileBase.
+ *
+ *  agileBase is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  agileBase is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with agileBase.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 function assignButtonModuleActions() {
 	$('button.moduleaction').click(function() {
 		var internalModuleName = $(this).attr('internalmodulename');
 		var actionName = $(this).attr('actionname');
-		document.location = "?return=gui/mobile/module_action&set_module=" + internalModuleName + "&set_custom_string=true&key=moduleaction&value=" + actionName;
+		document.location = "?return=gui/mobile/module_action&set_module=" + internalModuleName + "&set_custom_string=true&key=actionname&value=" + actionName;
 	});
 }
