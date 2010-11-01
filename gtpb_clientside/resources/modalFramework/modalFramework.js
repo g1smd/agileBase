a41ec0525876d3a25b225d23843bd410dc7e2544
Oliver Kohll <oliver@gtwm.co.uk>
2010-11-01 23:04:42 +0000

    iPad: wizard cancel action: debug

--- gtpb_clientside/resources/modalFramework/modalFramework.js
+++ gtpb_clientside/resources/modalFramework/modalFramework.js
@@ -141,8 +141,7 @@ function fShowModalDialog(sTemplateLocation, sCaption, fCallbackFn, sButtons, sA
   	    oBlank.parentNode.removeChild(oBlank);	
   	    // if on a mobile device, return to the home screen
   	    // - the dialog will have loaded in a separate page
-  	    alert('location is ' + document.location);
-  	    if(document.location.contains('gui/mobile/module_action')) {
+  	    if(document.location.indexOf('gui/mobile/module_action') > -1) {
   	    	document.location = '?return=boot_mobile';
   	    }
   	  }
