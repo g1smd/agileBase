a4c379aac9bc14706804ed6f72e5f29bf2f5b78b
Oliver Kohll <oliver@gtwm.co.uk>
2010-11-19 12:11:36 +0000

    Individual record printout: JS

--- gtpb_clientside/resources/tabs/tabs.js
+++ gtpb_clientside/resources/tabs/tabs.js
@@ -208,7 +208,7 @@ var TabInterfaceObjectPub = new Object();
 // Initialise
 $(document).ready(function(){
 	pane3TabInterface = new TabInterfaceObject($('.tab-interface')[0]);
-	if(fInit) {
+	if(typeof fInit != 'undefined') {
 	  pane3Scripts.functionList.push(fInit);
 	}
 	pane3Scripts.update();
