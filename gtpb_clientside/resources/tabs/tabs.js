c443e0bfec7fd072164c469a9d5f1aaab826dc75
Oliver Kohll <oliver@gtwm.co.uk>
2010-11-20 13:04:34 +0000

    Secondary display field support: using in picker debug

--- gtpb_clientside/resources/tabs/tabs.js
+++ gtpb_clientside/resources/tabs/tabs.js
@@ -324,7 +324,6 @@ function fRelationPickers() {
 					  selectFirst: true,
 					  width: 296,
 					  formatItem: function(rawValue, i, optionsDataLength) {
-					    alert(typeof rawValue);
 					    var formattedValue = rawValue[0].replace("{"," <span class='secondaryValue'>");
 					    formattedValue = formattedValue.replace("}","</span>");
 					    return formattedValue;
