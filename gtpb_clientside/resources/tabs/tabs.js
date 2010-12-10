35b18c2e641fea0d555e852364bd1fff460d00e1
Oliver Kohll <oliver@gtwm.co.uk>
2010-12-10 09:27:10 +0000

    JS error: send dates up when updateAsType is true, not false: debug

--- gtpb_clientside/resources/tabs/tabs.js
+++ gtpb_clientside/resources/tabs/tabs.js
@@ -426,7 +426,9 @@ function fKeyUpEvent(inputElement) {
   fSetValueAtt(jqWrapper[0]);
   var globalEdit = (jqWrapper.attr("gtpb_global_edit") != "undefined");
   var updateAsType = (jqWrapper.attr("update_as_type") != "undefined");
+  alert('globalEdit is ' + globalEdit + ', updateAsType is ' + updateAsType);
   if(!globalEdit && updateAsType) {
+	alert('writing');
 	top.oBuffer.writeBuffer(jqWrapper[0]);
   }
 }
@@ -439,7 +441,7 @@ function fChangeEvent(inputElement) {
   fSetValueAtt(jqWrapper[0]);
   var globalEdit = (jqWrapper.attr("gtpb_global_edit") != "undefined");
   var updateAsType = (jqWrapper.attr("update_as_type") != "undefined");
-  if(!globalEdit && updateAsType) {
+  if(!globalEdit && updateAsType) { 
 	new fChange(jqWrapper[0]);
   }
 }
