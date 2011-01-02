38ef285ba9637307c1909712e113673b4dcf2b5c
Oliver Kohll <oliver@gtwm.co.uk>
2011-01-02 21:33:41 +0000

    Deploy test CSS path fix

--- gtpb_clientside/deploy_test.sh
+++ gtpb_clientside/deploy_test.sh
@@ -7,7 +7,7 @@ ant -f deploy_templates_ubuntu.xml deploy
 # CSS
 #pane 1
 cat /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/tree/tree.css /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/module-colours.css /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/mobile/pane1_override.css > /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/mobile/pane1_amalgam.css
-cat /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/tree/tree.css /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/module-colours.css /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/styles/jquery.cluetip.css > /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/pane1_amalgam.css
+cat /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/tree/tree.css /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/module-colours.css /usr/local/tomcat/apache-tomcat/webapps/agileBase/styles/jquery.cluetip.css > /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/pane1_amalgam.css
 #pane 2
 cat /usr/local/tomcat/apache-tomcat/webapps/agileBase/styles/report.css /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/module-colours.css /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/mobile/override.css > /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/mobile/pane2_amalgam.css
 cat /usr/local/tomcat/apache-tomcat/webapps/agileBase/styles/report.css /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/module-colours.css > /usr/local/tomcat/apache-tomcat/webapps/agileBase/resources/pane2_amalgam.css
