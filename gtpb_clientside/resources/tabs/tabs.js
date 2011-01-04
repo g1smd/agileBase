a7ee44e2e2a24a7f2f764c11f65c8b958f5c648a
Oliver Kohll <oliver@gtwm.co.uk>
2011-01-04 12:22:23 +0000

    Date Picker: show currently selected date: debug

--- gtpb_clientside/resources/tabs/tabs.js
+++ gtpb_clientside/resources/tabs/tabs.js
@@ -536,6 +536,7 @@ function fDatePickers() {
 	  var month = $('select[wrapperAttribute="gtpb_' + internalFieldName + '_months' + '"]').val();
 	  var day = $('select[wrapperAttribute="gtpb_' + internalFieldName + '_days' + '"]').val();
 	  var currentDate = new Date(year, month - 1, day);
+	  alert('Currently selected: ' + currentDate);
 	  jqDateSelector.datePicker({startDate: '01/01/1901'}).bind('dateSelected',function(e, selectedDate, $td, status) {
 		  var day = selectedDate.getDate();
 		  var month = selectedDate.getMonth();
