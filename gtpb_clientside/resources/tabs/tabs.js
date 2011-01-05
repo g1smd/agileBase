edc3a9f6d9f7e6f45ef84c1405cea770d2c904ca
Oliver Kohll <oliver@gtwm.co.uk>
2011-01-05 21:20:01 +0000

    Don't set up a date picker multiple times: debug

--- gtpb_clientside/resources/tabs/tabs.js
+++ gtpb_clientside/resources/tabs/tabs.js
@@ -530,6 +530,7 @@ function fSetValueAtt(oWrapperDiv) {
 
 function fDatePickers() {
   $('.dp-choose-date').each(function() {
+	  alert(this.tagName);
 	  if (this.tagName == "A") {
 		  return
 	  }
