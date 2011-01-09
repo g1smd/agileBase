556f56935932cac9a9412fc4e42b42cead10ba84
Oliver Kohll <oliver@gtwm.co.uk>
2011-01-09 20:48:09 +0000

    IE / Firefox CSS glitch

--- gtpb_server/src/com/gtwm/pb/model/manageSchema/ReportSummaryDefn.java
+++ gtpb_server/src/com/gtwm/pb/model/manageSchema/ReportSummaryDefn.java
@@ -291,7 +291,6 @@ public class ReportSummaryDefn implements ReportSummaryInfo, Comparable<ReportSu
 		int rangePercent = this.getRangePercent();
 		if (rangePercent < 100) {
 			double rangeFraction = ((double) rangePercent) / 100;
-			logger.debug("" + rangePercent + " / 100 = " + rangeFraction);
 			sqlForSummary += " LIMIT (";
 			sqlForSummary += " SELECT (count(*) * " + rangeFraction + ")::integer";
 			sqlForSummary += " FROM " + this.getReport().getInternalReportName() + ")";
@@ -300,7 +299,6 @@ public class ReportSummaryDefn implements ReportSummaryInfo, Comparable<ReportSu
 		if (validSummary) {
 			reportData.fillInFilterValues(filtersUsed, statement);
 		}
-		logger.debug("Summary: " + statement);
 		return statement;
 	}
 
