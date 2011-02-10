2bca8036eba215b4589f8488aeb64c6d11fe3366
Oliver Kohll <oliver@gtwm.co.uk>
2011-02-10 10:02:09 +0000

    Try Google Feeds move

--- gtpb_clientside/website/scripts/ab.js
+++ gtpb_clientside/website/scripts/ab.js
@@ -1,30 +1,4 @@
 
-		function  loadFeedControl() {
-			var feed  = "http://blog.agilebase.co.uk/?feed=rss2";
-				var options = {
-				numResults : 6,
-						displayTime : 15000,
-						title : "<a href='http://blog.agilebase.co.uk'>announcements</a>",
-						linkTarget : google.feeds.LINK_TARGET_SELF
-				};
-			var fg = new GFdynamicFeedControl(feed, "announce", options);
-		}
-
-		function LoadSlideShow() {
-			// test known to work: http://feed.photobucket.com/images/sunsets/feed.rss
-			// flickr: http://api.flickr.com/services/feeds/photoset.gne?set=72157615624584156&nsid=36549361@N06&lang=en-us
-				var screenshotfeed  = "http://picasaweb.google.com/data/feed/base/user/oliver.kohll/albumid/5315354509188522353?alt=rss&kind=photo&hl=en_US";
-				var options = {
-						displayTime: 17000,
-						transistionTime: 500,
-						scaleImages: false,
-						maintainAspectRatio: true,
-						linkTarget: google.feeds.LINK_TARGET_BLANK,
-						thumbnailSize : GFslideShow.THUMBNAILS_LARGE,
-						pauseOnHover : false
-				};
-				var ss = new GFslideShow(screenshotfeed, "screenshots", options);
-		}
 
 		$(document).ready(function(){
 			// frame buster needed as we may be in one of the agileBase application panes after a login timeout
@@ -68,12 +42,3 @@
 			$("#" + sectionName).fadeIn("fast");
 		}
 
-		// Google stuff outside of jquery document.ready
-		try {
-			// start the RSS headlines from blog
-			google.setOnLoadCallback(loadFeedControl);
-			// start the RSS screenshots from flickr
-			google.setOnLoadCallback(LoadSlideShow);
-		} catch(err) {
-			alert(err);
-		}
