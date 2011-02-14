ada3ed58b4f350ffbd8cffbd0a5dd4addb54c7dd
Oliver Kohll <oliver@gtwm.co.uk>
2011-02-14 14:32:04 +0000

    Calendar feed URL test

---
+++ gtpb_clientside/resources/fullcalendar/calendar.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $('#calendar').fullCalendar({
+    header: {
+      left:   'title',
+      center: 'month,agendaWeek,agendaDay',
+      right:  'today prev,next'
+      eventSources: ['AppController.servlet?return=gui/calendar/feed&internaltablename=abc&internalreportname=xyz']
+	}
+  });
+  updateSelectedReport();
+});
+
+function updateSelectedReports() {
+  $("#selected_reports").children().remove();
+  $("#report_selection input:checked").each(function() {
+	var reportName = $(this).text();
+	#("#selected_reports").append("<span class='active_report'>" + reportName + "</span>");
+  });
+}
+
+$("#report_selection input").change(function {
+  var internalReportName = $(this).attr("internalreportname");
+  var reportName = $(this).text();
+  
+});
