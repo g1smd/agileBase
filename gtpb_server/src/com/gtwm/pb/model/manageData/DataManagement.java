76c71dfc96b65716ce07586f49982bb6eb0eefa3
Oliver Kohll <oliver@gtwm.co.uk>
2011-02-15 15:44:32 +0000

    Set midnight events to 'all day'

--- gtpb_server/src/com/gtwm/pb/model/manageData/DataManagement.java
+++ gtpb_server/src/com/gtwm/pb/model/manageData/DataManagement.java
@@ -1499,16 +1499,14 @@ public final class DataManagement implements DataManagementInfo {
 			js.key("id").value(internalReportName + "_" + reportDataRow.getRowId());
 			String eventTitle = buildCalendarEventTitle(report, reportDataRow);
 			js.key("title").value(eventTitle);
+			boolean allDayEvent = allDayValues;
 			if (!allDayValues) {
 				String eventDateDisplayValue = eventDateValue.getDisplayValue();
 				if (eventDateDisplayValue.trim().endsWith("00:00")) {
-					js.key("allDay").value(true);
-				} else {
-					js.key("allDay").value(false);
+					allDayEvent = true;
 				}
-			} else {
-				js.key("allDay").value(true);
 			}
+			js.key("allDay").value(allDayEvent);
 			Long eventDateEpoch = Long.parseLong(eventDateValue.getKeyValue()) / 1000;
 			js.key("start").value(eventDateEpoch);
 			js.key("end").value(eventDateEpoch + 600); // try 10 mins
