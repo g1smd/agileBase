771995534a60f48fb280b5648ef8c5f68a6e4083
Oliver Kohll <oliver@gtwm.co.uk>
2011-03-30 14:41:19 +0100

    Konqueror filtering testing

--- gtpb_clientside/resources/wait/request_setFilter.js
+++ gtpb_clientside/resources/wait/request_setFilter.js
@@ -479,5 +479,5 @@ var gtpbOldBrowserTimeout;
 
 function fReloadAfterDelay() {
   clearTimeout(gtpbOldBrowserTimeout);
-  gtpbOldBrowserTimeout = setTimeout("window.location.reload(true);", 4000);
+  gtpbOldBrowserTimeout = setTimeout("window.location.reload(true);", 3000);
 }
