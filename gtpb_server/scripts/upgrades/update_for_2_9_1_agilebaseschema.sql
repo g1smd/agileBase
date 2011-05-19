a4d6732c9cdf7addcfc32b42b5f1df193b34d5fc
Oliver Kohll <oliver@gtwm.co.uk>
2011-05-19 17:54:24 +0100

    Initial print/view redo: separator style

--- gtpb_server/scripts/upgrades/update_for_2_9_1_agilebaseschema.sql
+++ gtpb_server/scripts/upgrades/update_for_2_9_1_agilebaseschema.sql
@@ -1,3 +1,4 @@
 update abstractfield set printoutsetting ='NAME_AND_VALUE';
 update abstractfield set printoutsetting ='VALUE_ONLY' where internalfieldname in (SELECT internalfieldname FROM filefielddefn);
 update abstractfield set printoutsetting ='VALUE_ONLY' where internalfieldname in (SELECT internalfieldname FROM textfielddefn where useslookupdirect = false);
+update abstractfield set printoutsetting ='VALUE_ONLY' where internalfieldname in (SELECT internalfieldname FROM relationfielddefn);
