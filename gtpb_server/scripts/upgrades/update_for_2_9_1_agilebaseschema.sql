1d76bcb4b137df572cde17706604446b6734f91c
Oliver Kohll <oliver@gtwm.co.uk>
2011-05-19 17:31:46 +0100

    Initial print/view redo: h2 style

--- gtpb_server/scripts/upgrades/update_for_2_9_1_agilebaseschema.sql
+++ gtpb_server/scripts/upgrades/update_for_2_9_1_agilebaseschema.sql
@@ -1,1 +1,3 @@
 update abstractfield set printoutsetting ='NAME_AND_VALUE';
+update abstractfield set printoutsetting ='VALUE_ONLY' where internalfieldname in (SELECT internalfieldname FROM filefielddefn);
+update abstractfield set printoutsetting ='VALUE_ONLY' where internalfieldname in (SELECT internalfieldname FROM textfielddefn where useslookupdirect = false);
