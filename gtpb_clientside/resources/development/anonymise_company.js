18bd836902963b51fe7b1f5af35052333ac6a2eb
Oliver Kohll <oliver@gtwm.co.uk>
2011-06-09 11:47:38 +0100

    Complete company anonymisation script

--- gtpb_clientside/resources/development/anonymise_company.js
+++ gtpb_clientside/resources/development/anonymise_company.js
@@ -50,20 +50,219 @@ var gtwmJson = [{
 	aca70cd262c1a420c: 'OTHER', // interests
 	af752f749d68f6ad1: 'NOTES', // friends and colleagues
 	a182f3ed5cae2c47d: 'NOTES', // family
-	a8dc1652dcf676329: 'OTHER', // notes
+	a8dc1652dcf676329: 'OTHER' // notes
 },
 {
 	anonymise : true,
 	'return': 'blank',
-	set_table : 'abc1bfbb798d8ed70',
-	table_description: '...'
+	set_table : 'a16cb2d2ac204aaad',
+	table_description: 'staff',
+	a6838bb95dc578885: 'OTHER', // address
+	ad75348ec3ebf76cf: 'OTHER',
+	aadc6903536b6ff96: 'OTHER',
+	a150ad4df4565c3c2: 'OTHER',
+	abbebae9adb6f342a: 'PHONE_NUMBER',
+	a6411280b0c21f8f9: 'EMAIL_ADDRESS',
+	ab0c6920922cc29e5: 'OTHER',
+	ac1558c8dfd06793c: 'OTHER',
+	a504a83214cbddcaa: 'OTHER',
+	a10c53ec626a8fd2: 'NOTES', // next of kin
+	a4027570fb096c2a2: 'NOTES', // notes
+	v0hck1cuzf1ypdr3d: 'OTHER' // exit notes
+},
+{
+	anonymise : true,
+	'return': 'blank',
+	set_table : 'acc75c56f693811d4',
+	table_description: 'a5.2) resource library',
+	n87tngurdm9u29wd6: 'OTHER', // dept. / team
+	a61dfde59cead5332: 'NOTES', // resource name
+	a4620aa11ed7abe51: 'OTHER', // resource type
+	ae175f824e6330043: 'OTHER' // notes
+},
+{
+	anonymise : true,
+	'return': 'blank',
+	set_table : 'ae8d3a384687ad9c6',
+	table_description: 'b2.2) enquiries',
+	a5ba57da9f4c6b05c: 'OTHER', // source
+	adafc10ea45db24e4: 'COMPANY_NAME', // end client
+	a9c1b5ffed59f7605: 'OTHER', // opp manager
+	aa629614c458d78de: 'OTHER', // our role
+	a2b438061dd5a337c: 'OTHER', // notes
+	aabf1284843295d05: 'OTHER', // status
+	bpwqcym5atza3238y: 'OTHER', // project value
+	abdefbe52a26ef548: 'OTHER', // opp value
+	rbaxokxpbrjihncno: 'NOTES', // opp value logic
+	a38c9e5ab279eb80c: 'OTHER', // won / lost
+	ab4c0889143206d93: 'NOTES' // outcome
+},
+{
+	anonymise : true,
+	'return': 'blank',
+	set_table : 'scwuex91ctplyefcf',
+	table_description: 'b2.3) daily sales log',
+	dncs2uqspndetykb8: 'NOTES', // event record
+	xmj34tau3dygxlhab: 'NOTES', // client issues
+	gkzutxt9wra3b6rzj: 'NOTES', // our issues
+	n95um5rsmp9tccnn5: 'NOTES' // follow up actions
+},
+{
+	anonymise : true,
+	'return': 'blank',
+	set_table : 'a4f80b0b966b2c039',
+	table_description: 'b2.4) quotes',
+	ab55f3ef31662902e: 'OTHER', // proj. manager
+	a87afc6101e51ac5c: 'OTHER', // personnel
+	abb1256331de4a46e: 'NOTES', // location
+	af187f07b35f8c344: 'NOTES', // goal
+	a6b4639df64c7def1: 'NOTES', // task
+	mmsnhkrnmvfbsqpu6: 'OTHER' // narrative
+},
+{
+	anonymise : true,
+	'return': 'blank',
+	set_table : 'a9d18dec27c36855',
+	table_description: 'b2.5) commissions',
+	af25e12fcc2b2384f: 'OTHER', // day rate
+	a67f48a6002aa3ac4: 'NOTES', // locations
+	ad7af28a2b7dd4a75: 'NOTES' // original goal
+},
+{
+	anonymise : true,
+	'return': 'blank',
+	set_table : 'aeb343a06763bc1a2',
+	table_description: 'b3.2) projects',
+	a8d1b9f7577b722fe: 'COMPANY_NAME', // end client
+	//rqboextklzjhrwm4w: 'FULL_NAME', // proj. champion (dropdown)?
+	a424d4abbbee5ccc4: 'NOTES' // proj. brief
+},
+{
+	anonymise : true,
+	'return': 'blank',
+	set_table : 'ayzseqxstb0tbokkr',
+	table_description: 'b3.6) business cases',
+	fgq3wdbdejujbtw3o: 'NOTES', // reasons
+	dhr54p4bqovuwnqwp: 'NOTES', // options considered
+	ybfl9ozoescsqnvcx: 'NOTES', // evaluation
+	sarm1sbqnfhj5ch0i: 'NOTES', // proposal
+	bmajz6b8sauhjr6yr: 'NOTES', // expected benefit
+	b5kmwnjwdkcml5uq1: 'NOTES', // timescale
+	gbdt1cm6ictsc9wzb: 'NOTES' // investment appraisal
+},
+{
+	anonymise : true,
+	'return': 'blank',
+	set_table : 'a3cde33f812373d03',
+	table_description: 'b3.8) user req.',
+	a1d83745a557cb275: 'OTHER', // title
+	a5c994b6802921b5a: 'OTHER', // who
+	a82dc96fece7c6af9: 'OTHER' //what
+	
+},
+{
+	anonymise : true,
+	'return': 'blank',
+	set_table : 'a7d7814185004ee4d',
+	table_description: 'b4.1) glossary',
+	a5979e36e4d7381cb: 'OTHER', // word
+	a438ba3d695f779ce: 'OTHER' // usage	
+},
+{
+	anonymise : true,
+	'return': 'blank',
+	set_table : 'afeff8cee435c68ba',
+	table_description: 'b4.3) tasks',
+	aaef2b8a16274ae26: 'OTHER', // task
+	a438ba3d695f779ce: 'OTHER' // usage	
+},
+{
+	anonymise : true,
+	'return': 'blank',
+	set_table : 'aba44ae98114f38fc',
+	table_description: 'b5.2) dev. programmes',
+	a3b0a05971a82c6ee: 'NOTES'
+},
+{
+	anonymise : true,
+	'return': 'blank',
+	set_table : 'ace05a675a5fd1985',
+	table_description: 'b5.3) dev. product versions',
+	a4dbaeca51fe8ad4e: 'NOTES', // version
+	a8b49ccb73371933: 'NOTES' // description
+},
+{
+	anonymise : true,
+	'return': 'blank',
+	set_table : 'aee9b2492157fb779',
+	table_description: 'b5.4) dev. issue tracking',
+	aa458eff3974f654f: 'OTHER', // summary
+	a579994f578810876: 'OTHER' // description
+},
+{
+	anonymise : true,
+	'return': 'blank',
+	set_table : 'ac6fd0f01c7f7b0b3',
+	table_description: 'b6.0) client support log',
+	a3e258b1794404deb: 'OTHER', // title
+	ab08c0fef8ae46b37: 'NOTES' // description
+},
+{
+	anonymise : true,
+	'return': 'blank',
+	set_table : 'aff6022675e2299c8',
+	table_description: 'b7.1) timesheet,
+	a94e46c316ea5b0c0: 'NOTES' // notes
+},
+{
+	anonymise : true,
+	'return': 'blank',
+	set_table : 'aff6022675e2299c8',
+	table_description: 'b7.1) timesheet,
+	a94e46c316ea5b0c0: 'NOTES' // notes
+},
+{
+	anonymise : true,
+	'return': 'blank',
+	set_table : 's0mhcrftheqfetlzd',
+	table_description: 'c1.1) email metrics,
+	uwncovushaiq0v8rb: 'OTHER', // opens
+	jnejxk7upwfq1lffz: 'OTHER' // clickthroughs
+},
+{
+	anonymise : true,
+	'return': 'blank',
+	set_table : 'afa0be55b91ebf715',
+	table_description: 'c3.0) purchase invoices,
+	ad02f2dc798bbcfc8: 'OTHER', // external ref
+	a28868f37c860801f: 'OTHER', // total
+	a113e422d7206b273: 'NOTES' // notes
+},
+{
+	anonymise : true,
+	'return': 'blank',
+	set_table : 'a584d5131d5744c0',
+	table_description: 'c3.1) expenses,
+	ae3ef48ceb586caa5: 'OTHER', // claimant
+	a313cae2c6fd45e83: 'COMPANY_NAME' // payee
+},
+{
+	anonymise : true,
+	'return': 'blank',
+	set_table : 'a635b46ea42d4d4d0',
+	table_description: 'c4.1) billing,
+	a7d41586083a81733: 'OTHER', // amount
+	aaae6b9240603b247: 'NOTES', // notes
+	bx0rdjzijharzzkq3: 'NOTES' // internal notes
 }];
 
 // a recursive function that moves on to the next table on completion of the
 // previous
 function anonymiseCompany(tableIndex) {
   	tableCommand = gtwmJson[tableIndex];
-  	$("#results").append("Anonymising...<br>");
+  	if (tableIndex == 0) {
+  	  $("#results").append("Anonymising...<br>");
+  	}
   	$.post("AppController.servlet", tableCommand, function() {
   		$("#results").append("Table anonymised: " + tableCommand.table_description + "<br>");
   		var newTableIndex = tableIndex + 1;
