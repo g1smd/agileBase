d196c38faeb9347913e0a0709689284881627860
Oliver Kohll <oliver@gtwm.co.uk>
2011-06-20 16:25:49 +0100

    Slide nav JS fix

--- gtpb_clientside/resources/edit_nav/edit_nav.js
+++ gtpb_clientside/resources/edit_nav/edit_nav.js
@@ -311,7 +311,7 @@ function initialiseDependencies() {
 							relationClick(jqDependentTable);
 						} else {
 							// find index of slide to go to
-							var slideId jqDependentTable.attr("id").replace("dependent_table_","slide_");
+							var slideId = jqDependentTable.attr("id").replace("dependent_table_","slide_");
 							var slideNum = $("#" + slideId).index();
 							slideshow.go(slideNum + 1);
 							$(".presentation").scrollTop(0);
