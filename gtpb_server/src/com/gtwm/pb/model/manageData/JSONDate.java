79c5bbddc09ffb1aa2c28bd5ceb7c1f90a729630
Oliver Kohll <oliver@gtwm.co.uk>
2011-07-27 17:12:05 +0100

    chart toString

--- gtpb_server/src/com/gtwm/pb/model/manageData/JSONDate.java
+++ gtpb_server/src/com/gtwm/pb/model/manageData/JSONDate.java
@@ -6,20 +6,24 @@ import org.json.JSONString;
  * Allow outputting of raw Javascript Date objects using JSON.
  * 
  * JSON doesn't technically have a date type but JSON interpreted by JS, e.g.
- * Simile Timeplot will recognise JS dates
+ * Simile Timeplot will recognise JS dates and parse the JSON quicker
  * 
- * @see http://www.simile-widgets.org/wiki/Timeline_EventSources#Date_Time_Formats
+ * @see http
+ *      ://www.simile-widgets.org/wiki/Timeline_EventSources#Date_Time_Formats
  */
 public class JSONDate implements JSONString {
 
+	/**
+	 * @param milliseconds
+	 *            Milliseconds since the epoch
+	 */
 	public JSONDate(long milliseconds) {
 		this.milliseconds = milliseconds;
 	}
-	
+
 	public String toJSONString() {
-		return "new Date(" + milliseconds + ")";
+		return "new Date(" + this.milliseconds + ")";
 	}
 
 	private final long milliseconds;
 }
-
