3333cad6c4083464d010a40aa2b9f575fe7aa329
Oliver Kohll <oliver@gtwm.co.uk>
2011-07-29 10:46:51 +0100

    Save plain text content of editable divs in A3 report: debug

--- gtpb_clientside/resources/wait/editBuffer_editData.js
+++ gtpb_clientside/resources/wait/editBuffer_editData.js
@@ -572,6 +572,7 @@ function fChange(oObj, postChangeFunction)  {
   	  // if jsHtmlToText library is present
   	  if (typeof htmlToText == "function") {
   	  	vValue = htmlToText(new String(vValue));
+  	  	alert("saving " + vValue);
   	  }
     } else {
       // TODO: should this line be exactly the same as the similar one earlier?
