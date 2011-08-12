db90d912e669b2a571b937db6708124e152181da
Oliver Kohll <oliver@gtwm.co.uk>
2011-08-12 13:23:14 +0100

    When saving phone numbers, make sure they include a space so spreadsheets don't mis-recognise them as numbers when exported

--- gtpb_server/src/com/gtwm/pb/servlets/ServletSessionMethods.java
+++ gtpb_server/src/com/gtwm/pb/servlets/ServletSessionMethods.java
@@ -716,7 +716,7 @@ public final class ServletSessionMethods {
 						// Format phone numbers to include a space so that when they're exported to CSV, spreadsheets recognise them as text rather than numbers
 						if ((new TextValueDefn(fieldValueString)).isPhoneNumber()) {
 							if (!fieldValueString.contains(" ")) {
-								fieldValueString = fieldValueString.substring(0, 4) + " " + fieldValueString.substring(5);
+								fieldValueString = fieldValueString.substring(0, 5) + " " + fieldValueString.substring(5);
 							}
 						}
 						fieldValue = new TextValueDefn(fieldValueString);
