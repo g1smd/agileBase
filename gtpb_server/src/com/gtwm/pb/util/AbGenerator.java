166e6cc9b22de47df14a28ec8f0a68616ba3445f
Oliver Kohll <oliver@gtwm.co.uk>
2011-09-11 11:01:39 +0100

    Re-enable deleting pkeys in reports

--- gtpb_server/src/com/gtwm/pb/util/AbGenerator.java
+++ gtpb_server/src/com/gtwm/pb/util/AbGenerator.java
@@ -3,32 +3,29 @@ package com.gtwm.pb.util;
 import org.jwalk.GeneratorException;
 import org.jwalk.gen.CustomGenerator;
 import org.jwalk.gen.MasterGenerator;
-import org.jwalk.gen.StringGenerator;
 import com.gtwm.pb.auth.Company;
 import com.gtwm.pb.model.interfaces.CompanyInfo;
 
 public class AbGenerator implements CustomGenerator {
 
+	private MasterGenerator owner;
+
 	@Override
-	public boolean canCreate(Class<?> testClass) {
-		if (testClass.equals(CompanyInfo.class)) {
-			return true;
-		}
-		return false;
+	public boolean canCreate(Class<?> type) {
+		return type == CompanyInfo.class;
 	}
 
 	@Override
-	public Object nextValue(Class<?> testClass) throws GeneratorException {
-		if (testClass.equals(CompanyInfo.class)) {
-			return new Company((String) new StringGenerator().nextValue(String.class));
-		}
-		throw new GeneratorException(testClass);
+	public Object nextValue(Class<?> type) throws GeneratorException {
+		if (type == CompanyInfo.class)
+			return owner.nextValue(Company.class);
+		else
+			throw new GeneratorException(type);
 	}
 
 	@Override
-	public void setOwner(MasterGenerator testClass) {
-		// TODO Auto-generated method stub
-
+	public void setOwner(MasterGenerator generator) {
+		owner = generator;
 	}
 
 }
