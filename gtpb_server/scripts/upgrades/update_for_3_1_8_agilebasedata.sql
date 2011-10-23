d16115c14ff3d00169e455c4bd0b77f4e167d907
Oliver Kohll <oliver@gtwm.co.uk>
2011-10-23 18:48:01 +0100

    Initial clientside support for comments

---
+++ gtpb_server/scripts/upgrades/update_for_3_1_8_agilebasedata.sql
@@ -0,0 +1,9 @@
+CREATE TABLE dbint_comments(
+  created timestamp,
+  comment varchar(100000),
+  author varchar(10000),
+  internalfieldname varchar(1000),
+  rowid integer
+);
+
+create index comments_idx on dbint_comments(internalfieldname, rowid);
