6a83de801a3c73a09aaba80f884a0c9c120d8d08
Oliver Kohll <oliver@gtwm.co.uk>
2012-01-05 15:44:33 +0000

    Refactor interdependent fields: debug

--- gtpb_clientside/resources/tabs/tabs.js
+++ gtpb_clientside/resources/tabs/tabs.js
@@ -1069,7 +1069,7 @@ function fInitialiseDependencies() {
 	var lastUsedParentNameIndexes = new Object();
 	//var aSelect = document.getElementsByTagName('SELECT');
 	var aSelect = $.makeArray($('.interdependent select'));
-	alert("No. selects: " + aSelect.length);
+	alert("No. selects now: " + aSelect.length);
 	// for every select
 	for ( var iSelect = 0; iSelect < aSelect.length; iSelect++) {
 		var selectName = aSelect[iSelect].name;
