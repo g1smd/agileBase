3e372711e9b37f15d85ede5726a257f1944bb8db
Oliver Kohll <oliver@gtwm.co.uk>
2012-01-20 15:15:31 +0000

    Two-col: comments

--- gtpb_clientside/resources/tabs/tabs.js
+++ gtpb_clientside/resources/tabs/tabs.js
@@ -961,8 +961,8 @@ function fComments() {
 		}
 	})
 	$(".comment_toggle").click(function() {
-		$(this).next(".add_comment_row").show("normal");
-		$(this).next(".add_comment_row").find(".comment_input").focus();
+		$(this).nextUntil(".add_comment_row").next().show("normal");
+		$(this).nextUntil(".add_comment_row").next().find(".comment_input").focus();
 	});
 }
 
