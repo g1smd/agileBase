ea3ae23ab2ac90c4119a800cd204eb5e2a29248b
Oliver Kohll <oliver@gtwm.co.uk>
2012-02-01 20:42:36 +0000

    Fullscreen test

--- gtpb_clientside/resources/button/button.js
+++ gtpb_clientside/resources/button/button.js
@@ -128,7 +128,9 @@ function fLinks() {
 
 function fPrint() {
 	alert('rowsWrapper');
-	document.getElementById('oViewPane').contentWindow.document.getElementById('colsWrapper').contentDocument.getElementById('rowsWrapper').mozRequestFullScreen();
+	var jqViewPane = $("#oViewPane");
+	var jqRowsWrapper = jqViewPane.find("#rowsWrapper");
+	jqRowsWrapper[0].mozRequestFullScreen();
   //var oPrintWin=window.spawnWindow('AppController.servlet?return=gui/printouts/pane2_printout_wrapper','print_window','toolbar=no,location=no,directories=no,status=no,copyhistory=no,menubar=no,resizable=yes,dialog=yes')	
 }
 
