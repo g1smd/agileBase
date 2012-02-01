4e311b2ffa09e9ac12ca35ecd5fcec92a314a221
Oliver Kohll <oliver@gtwm.co.uk>
2012-02-01 20:17:35 +0000

    Fullscreen test

--- gtpb_clientside/resources/button/button.js
+++ gtpb_clientside/resources/button/button.js
@@ -127,7 +127,7 @@ function fLinks() {
 }
 
 function fPrint() {
-	document.getElementById('oViewPane').contentWindow.pane_2.mozRequestFullScreen();
+	document.getElementById('oViewPane').contentWindow.mozRequestFullScreen();
   //var oPrintWin=window.spawnWindow('AppController.servlet?return=gui/printouts/pane2_printout_wrapper','print_window','toolbar=no,location=no,directories=no,status=no,copyhistory=no,menubar=no,resizable=yes,dialog=yes')	
 }
 
