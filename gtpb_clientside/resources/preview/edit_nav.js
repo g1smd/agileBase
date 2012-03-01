173f0e200e97fabb783445194ed9f14440228ffb
Oliver Kohll <oliver@gtwm.co.uk>
2012-03-01 17:56:26 +0000

    Load from preview dev.

--- gtpb_clientside/resources/preview/edit_nav.js
+++ gtpb_clientside/resources/preview/edit_nav.js
@@ -1,37 +1,34 @@
-$(document).ready(
-	function() {
-		$(".searchbox").live(
-				'keyup',
-				function() {
-					var jqSearchBox = $(this);
-					jqSearchBox.addClass("changed");
-					var filterString = jqSearchBox.val();
-					var internalReportName = $("#searchbox").attr(
-							"internalreportname");
-					$.get(
-							"AppController.servlet?return=gui/preview/report_content&set_report="
-									+ internalReportName
-									+ "&set_global_report_filter_string=true&filterstring="
-									+ filterString, function(data) {
-								// response has come back from the
-								// server, check it isn't out of
-								// date
-								if (jqSearchBox.val() != filterString) {
-									return;
-								}
-								$("#homeContent").html(data);
-								jqSearchBox.removeClass("changed");
-							});
-				});
+$(document).ready(function() {
+	$(".searchbox").live(
+		'keyup',
+		function() {
+			var jqSearchBox = $(this);
+			jqSearchBox.addClass("changed");
+			var filterString = jqSearchBox.val();
+			var internalReportName = $("#searchbox").attr(
+					"internalreportname");
+			$.get(
+				"AppController.servlet?return=gui/preview/report_content&set_report="
+						+ internalReportName
+						+ "&set_global_report_filter_string=true&filterstring="
+						+ filterString, function(data) {
+					// response has come back from the
+					// server, check it isn't out of
+					// date
+					if (jqSearchBox.val() != filterString) {
+						return;
+					}
+					$("#homeContent").html(data);
+					jqSearchBox.removeClass("changed");
+			});
+		});
+});
 
-	});
-
-function bigImage(oImg) {
-	var jqImg = $(oImg);
-	var src = jqImg.attr("src");
-	src = src.replace(/\.500\.(jpg|png)/,'');
-	jqImg.attr("src", src);
-	var block = jqImg.closest(".block");
-	block.css('width','500px');
-	block.css('height','300px');
+function loadFromPreview(oBlock) {
+	var jqBlock = $(oBlock);
+	var rowId = jqBlock.attr("data-rowid");
+	// Find the row in pane 2, click it
+	var jqPane2 = $(document.getElementById('oViewPane').contentWindow.pane_2);
+	var jqRow = jqPane2.find("tr[name=" + rowId + "]");
+	alert("row " + jqRow.attr("name"));
 }
