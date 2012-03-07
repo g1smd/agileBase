4dda4c65913a273986b3dd0deb4dd0b7cfab9fd5
Oliver Kohll <oliver@gtwm.co.uk>
2012-03-07 22:10:00 +0000

    Search bar JS

--- gtpb_clientside/resources/preview/edit_nav.js
+++ gtpb_clientside/resources/preview/edit_nav.js
@@ -1,27 +1,24 @@
 $(document).ready(function() {
-	$(".searchbox").live(
-		'keyup',
-		function() {
-			var jqSearchBox = $(this);
-			jqSearchBox.addClass("changed");
-			var filterString = jqSearchBox.val();
-			var internalReportName = $("#searchbox").attr(
-					"internalreportname");
-			$.get(
-				"AppController.servlet?return=gui/preview/report_content&set_report="
-						+ internalReportName
-						+ "&set_global_report_filter_string=true&filterstring="
-						+ filterString, function(data) {
-					// response has come back from the
-					// server, check it isn't out of
-					// date
-					if (jqSearchBox.val() != filterString) {
-						return;
-					}
-					$("#homeContent").html(data);
-					jqSearchBox.removeClass("changed");
-			});
+	$(".searchbox").keyup(function() {
+		alert("search");
+		var jqSearchBox = $(this);
+		jqSearchBox.addClass("changed");
+		var filterString = jqSearchBox.val();
+		//var internalReportName = $("#searchbox").attr("internalreportname");
+		$.get(
+			"AppController.servlet?return=gui/preview/report_content"
+					+ "&set_global_report_filter_string=true&filterstring="
+					+ filterString, function(data) {
+				// response has come back from the
+				// server, check it isn't out of
+				// date
+				if (jqSearchBox.val() != filterString) {
+					return;
+				}
+				$("#homeContent").html(data);
+				jqSearchBox.removeClass("changed");
 		});
+	});
 	$(".searchbox").focus(function() {
 		fFullScreen();
 	});
