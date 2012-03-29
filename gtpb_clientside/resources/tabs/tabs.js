dabb2a6131e99cf28172185a10bcd0db2e4e2bea
Oliver Kohll <oliver@gtwm.co.uk>
2012-03-29 14:32:55 +0100

    Grandchild click event: debug

--- gtpb_clientside/resources/tabs/tabs.js
+++ gtpb_clientside/resources/tabs/tabs.js
@@ -1123,6 +1123,7 @@ function fInitialiseDependencies() {
  * Used when clicking on a selector report in a tab
  */
 function loadIntoTabTable(oRow, internalTableName, rowId) {
+	alert("Loading " + rowId);
 	var jqSelector = $(oRow).closest(".selectorReport");
 	if (jqSelector.closest(".block").hasClass("grandchildren")) {
 		alert("gc");
