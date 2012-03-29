621d2ce75fa0fd9cb8fb3163bfc675213c169b86
Oliver Kohll <oliver@gtwm.co.uk>
2012-03-29 16:53:00 +0100

    Try pane2/3 mutually exclusive

--- gtpb_clientside/resources/pane2.js
+++ gtpb_clientside/resources/pane2.js
@@ -210,7 +210,9 @@ function showPane3IfNecessary(oEvent) {
 	var jqButt = $(top.document).find("#pane3butt")
 	if (!jqButt.hasClass("selected")) {
 		jqButt.click();
-		$(top.document).find("#pane2butt").click();
+		if (jqButt.hasClass("REPORT")) {
+		  $(top.document).find("#pane2butt").click();
+		}
 	}
 }
 
