ff5daa0eeba9907c9d2f332ca7d921cab6524551
Oliver Kohll <oliver@gtwm.co.uk>
2012-05-03 16:18:58 +0100

    Set to edit or view tab when a report is clicked in pane 1: debug

--- gtpb_clientside/resources/pane2.js
+++ gtpb_clientside/resources/pane2.js
@@ -282,6 +282,7 @@ function loadIntoPane3(url, rowId, numberOfTabsExpected) {
 						var targetTab = $(parent.pane_3.document).find("ul.tab-list > li:eq(" + targetTabNum + ")");
 						alert(targetTab.text());
 						if (targetTab.find("strong").size() == 0) {
+							alert("not strong");
 							targetTab.click();
 						}
 					} catch (err) {
