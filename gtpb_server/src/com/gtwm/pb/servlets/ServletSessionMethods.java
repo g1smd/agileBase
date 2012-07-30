a2338131f47e71b816f104197f305deac9c286aa
g1smd <g1smd.email@gmail.com>
2012-07-30 19:02:08 +0000

    International.

--- gtpb_server/src/com/gtwm/pb/servlets/ServletSessionMethods.java
+++ gtpb_server/src/com/gtwm/pb/servlets/ServletSessionMethods.java
@@ -749,7 +749,7 @@ public final class ServletSessionMethods {
 //logger.debug("z16: we are now here");
 							}
 						// International phone numbers
-						/* Uncomment when TextValue.isPhoneNumberInternational implemented
+						// Uncomment when TextValue.isPhoneNumberInternational implemented
 						} else if ((new TextValueDefn(fieldValueString)).isPhoneNumberInternational()) {
 							fieldValueString = fieldValueString.replaceAll("(?:0(?:0\\s?|11\\s)|\\+)([1-9]\\d+).*", "$1");
 							if (!fieldValueString.matches(".*\\D.*")) {
@@ -757,7 +757,7 @@ public final class ServletSessionMethods {
 								fieldValueString = formatPhoneNumberInternational(fieldValueString);
 							}
 							fieldValueString = "+" + fieldValueString;
-						*/
+						//
 						} else {
 							// Replace smart quotes with normal quotes and em
 							// dashes with normal dashes
