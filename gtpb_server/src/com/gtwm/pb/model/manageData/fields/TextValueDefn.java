ac14299c28b2411d104f039ffaac6cf4badac5b7
g1smd <g1smd.email@gmail.com>
2012-09-16 16:19:19 +0000

    Simplify RegEx; less nesting.

--- gtpb_server/src/com/gtwm/pb/model/manageData/fields/TextValueDefn.java
+++ gtpb_server/src/com/gtwm/pb/model/manageData/fields/TextValueDefn.java
@@ -118,19 +118,19 @@ public class TextValueDefn implements TextValue {
 		if ((length > 9) && (length < 26)) {
 			// vaguely based on a regex from http://www.regexlib.com/
 			// alterations by @g1smd
-			// "^(?:(?:(?:0(?:0|11)\\s?|\\+)4\\s?4\\s?(?:\\(?0\\)?\\s?)?)|(?:\\(?0))(?:(?:\\d{5}\\)?\\s?\\d{4,5})|(?:\\d{4}\\)?\\s?(?:\\d{5}|\\d{3}\\s?\\d{3}))|(?:\\d{3}\\)?\\s?\\d{3}\\s?\\d{3,4})|(?:\\d{2}\\)?\\s?\\d{4}\\s?\\d{4}))(?:\\s?[x\\#]\\d{3,4})?$"
+			// "^(?:(?:0(?:0|11)\\s?|\\+)4\\s?4\\s?(?:\\(?0\\)?\\s?)?|\\(?0)(?:";\\d{5}\\)?\\s?\\d{4,5}|\\d{4}\\)?\\s?(?:\\d{5}|\\d{3}\\s?\\d{3})|\\d{3}\\)?\\s?\\d{3}\\s?\\d{3,4}|\\d{2}\\)?\\s?\\d{4}\\s?\\d{4})(?:\\s?[x\\#]\\d{3,4})?$"
 			String regexGB = "^";
 			regexGB += "(?:";
-			regexGB += "(?:(?:0(?:0|11)\\s?|\\+)4\\s?4\\s?(?:\\(?0\\)?\\s?)?)|";	// leading 00, 011 or + before 44 with optional (0); parentheses and spaces optional
-			regexGB += "(?:\\(?0)";													// leading (0, 0
+			regexGB += "(?:0(?:0|11)\\s?|\\+)4\\s?4\\s?(?:\\(?0\\)?\\s?)?|";	// leading 00, 011 or + before 44 with optional (0); parentheses and spaces optional
+			regexGB += "\\(?0";													// leading (0, 0
 			regexGB += ")";
 			regexGB += "(?:";
-			regexGB += "(?:\\d{5}\\)?\\s?\\d{4,5})|";						// [5+4]/[5+5]
-			regexGB += "(?:\\d{4}\\)?\\s?(?:\\d{5}|\\d{3}\\s?\\d{3}))|";	// [4+5]/[4+6]
-			regexGB += "(?:\\d{3}\\)?\\s?\\d{3}\\s?\\d{3,4})|";				// [3+6]/[3+7]
-			regexGB += "(?:\\d{2}\\)?\\s?\\d{4}\\s?\\d{4})";				// [2+8]
+			regexGB += "\\d{5}\\)?\\s?\\d{4,5}|";						// [5+4][5+5]
+			regexGB += "\\d{4}\\)?\\s?(?:\\d{5}|\\d{3}\\s?\\d{3})|";	// [4+5][4+6]
+			regexGB += "\\d{3}\\)?\\s?\\d{3}\\s?\\d{3,4}|";				// [3+6][3+7]
+			regexGB += "\\d{2}\\)?\\s?\\d{4}\\s?\\d{4}";				// [2+8]
 			regexGB += ")";
-			regexGB += "(?:\\s?[x\\#]\\d{3,4})?";					// optional "#" and extension
+			regexGB += "(?:\\s?[x\\#]\\d{3,4})?";						// optional "x" or "#" and extension
 			regexGB += "$";
 			if (this.textValue.trim().matches(regexGB)) {
 				return true;
