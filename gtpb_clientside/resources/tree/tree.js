47420f480de330f5eca668cbcfca359c9487f45f
Oliver Kohll <oliver@gtwm.co.uk>
2012-09-23 22:31:04 +0100

    Dependent parent modules test: show: debug

--- gtpb_clientside/resources/tree/tree.js
+++ gtpb_clientside/resources/tree/tree.js
@@ -220,7 +220,6 @@ function pane1Setup() {
 }
 
 function expandRelated() {
-	alert('expandRelated');
 	$("li.moduleexpanded:visible, li.modulecollapsed:visible").each(function() {
 		var relatedModules = $(this).attr("data-dependent").split(/\s+/);
 		for (i = 0; i < relatedModules.length; i++) {
