aa9b7b2ac54388660fa78fa7e9bf2a67089247f3
Oliver Kohll <oliver@gtwm.co.uk>
2012-10-03 08:13:39 +0100

    Presenter separate CSS, JS

---
+++ gtpb_clientside/resources/preview/argument.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	$(".card").click(function() {
+	  var container = $(this).closest(".container");
+	  var left = container.position().left;
+	  console.log(left);
+	  if(container.hasClass("zoomed")) {
+	    $(this).toggleClass("flipped");
+	    container.addClass("semi");
+	    $(".container").not(container).addClass("unzoomed");
+	    setTimeout(function() {
+	      container.removeClass("semi");
+	    }, 500);
+	  } else {
+	    if (left < 400) {
+	      container.css("left","400px");
+	    } else if (left < 800) {
+	      container.css("left","100px");
+	    }
+	    var img = $(this).find("img.image");
+	    img.attr("src", img.attr("data-bigsrc"));
+	    container.addClass("zoomed");
+	  }
+	});
+});
