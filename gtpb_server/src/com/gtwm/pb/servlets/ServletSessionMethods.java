7ce0cb4d174917ec5da10d6ea50ee96f24d9c7f1
Oliver Kohll <oliver@gtwm.co.uk>
2012-10-28 14:42:06 +0000

    Decode filter parameter debug

--- gtpb_server/src/com/gtwm/pb/servlets/ServletSessionMethods.java
+++ gtpb_server/src/com/gtwm/pb/servlets/ServletSessionMethods.java
@@ -1062,6 +1062,7 @@ public final class ServletSessionMethods {
 		}
 		try {
 			fieldValue = URLDecoder.decode(fieldValue, "UTF-8");
+			logger.debug("Decoded filter: " + fieldValue);
 		} catch (UnsupportedEncodingException ueex) {
 			throw new CodingErrorException("Unable to decode string: " + ueex);
 		}
