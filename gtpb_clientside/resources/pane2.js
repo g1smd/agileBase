ff02134e60ed18ecbed473372a17bc07aec1f7a5
Oliver Kohll <oliver@gtwm.co.uk>
2013-01-03 11:38:45 +0000

    Initial date filter css

--- gtpb_clientside/resources/pane2.js
+++ gtpb_clientside/resources/pane2.js
@@ -370,6 +370,7 @@ function clearFilters() {
 function dateFilterControls(event, inputObj) {
 	$("#fieldFilterControls").show();
 	$("#individualDateSelector").calendarPicker({
+		days: 31,
 		callback: function(cal) {
 			alert(cal.currentDate);
 		}
