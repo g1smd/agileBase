b253458cf1b668eb5ab56eff170101e8211a9925
Oliver Kohll <oliver@gtwm.co.uk>
2013-01-03 11:42:23 +0000

    Initial date filter sizing

--- gtpb_clientside/resources/pane2.js
+++ gtpb_clientside/resources/pane2.js
@@ -370,7 +370,7 @@ function clearFilters() {
 function dateFilterControls(event, inputObj) {
 	$("#fieldFilterControls").show();
 	$("#individualDateSelector").calendarPicker({
-		days: 7,
+		days: 14,
 		callback: function(cal) {
 			alert(cal.currentDate);
 		}
