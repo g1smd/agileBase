fe747b2e5c27fb8ed3d3f25ba9e2ad0eba1ddf38
Oliver Kohll <oliver@gtwm.co.uk>
2013-01-04 19:50:53 +0000

    Treemap borders test

--- gtpb_clientside/resources/simple/simple.js
+++ gtpb_clientside/resources/simple/simple.js
@@ -46,7 +46,7 @@ $(document).ready(function() {
 		      style.display = '';  
 		      style.border = '1px solid transparent';  
 		      domElement.onmouseover = function() {  
-		        style.border = '1px solid #EC00BC';  
+		        style.border = '2px solid #EC00BC';  
 		      };  
 		      domElement.onmouseout = function() {  
 		        style.border = '1px solid transparent';  
