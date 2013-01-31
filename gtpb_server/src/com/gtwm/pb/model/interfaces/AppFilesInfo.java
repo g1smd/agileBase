666a81dcb8aec02a5b04da65d12846a614227df6
Oliver Kohll <oliver@gtwm.co.uk>
2013-01-31 14:41:04 +0000

    FilesApp

---
+++ gtpb_server/src/com/gtwm/pb/model/interfaces/AppFilesInfo.java
@@ -0,0 +1,8 @@
+package com.gtwm.pb.model.interfaces;
+
+/**
+ * Used to present image and PDF other files
+ */
+public interface AppFilesInfo extends AppDataLinkInfo {
+
+}
---
+++ gtpb_server/src/com/gtwm/pb/model/manageSchema/apps/FilesApp.java
@@ -0,0 +1,39 @@
+package com.gtwm.pb.model.manageSchema.apps;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Transient;
+import com.gtwm.pb.model.interfaces.AppFilesInfo;
+import com.gtwm.pb.model.interfaces.BaseReportInfo;
+import com.gtwm.pb.model.manageSchema.BaseReportDefn;
+import com.gtwm.pb.util.RandomString;
+
+@Entity
+public class FilesApp extends AbstractApp implements AppFilesInfo {
+
+	public FilesApp(String colour, BaseReportInfo report) {
+		super.setColour(colour);
+		super.setInternalAppName(RandomString.generate());
+		this.setReport(report);
+	}
+
+	@Transient
+	public String getAppName() {
+		return this.getReport().getParentTable().getSimpleName();
+	}
+
+	@ManyToOne(targetEntity = BaseReportDefn.class)
+	public BaseReportInfo getReport() {
+		return this.report;
+	}
+
+	public void setReport(BaseReportInfo report) {
+		this.report = report;		
+	}
+	
+	public String toString() {
+		return this.getAppName();
+	}
+
+	private BaseReportInfo report = null;
+}
