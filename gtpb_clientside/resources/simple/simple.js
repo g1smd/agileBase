29593179ffb956b604dddc86a9222f9e39dd29b7
Oliver Kohll <oliver@gtwm.co.uk>
2013-02-11 16:02:28 +0000

    Save tile icon

--- gtpb_clientside/resources/simple/simple.js
+++ gtpb_clientside/resources/simple/simple.js
@@ -112,6 +112,7 @@ function addDataTile(selectedApp, colour, internalReportName, icon) {
 		add_tile : true,
 		tiletype : selectedApp,
 		colour : colour,
+		icon: icon,
 		internalreportname : internalReportName
 	}, function(data) {
 		$("#tiles").html(data);
