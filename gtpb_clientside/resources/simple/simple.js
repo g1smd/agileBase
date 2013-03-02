6685dedd0c056fe71eafbf4c3435f85d52661fa7
Oliver Kohll <oliver@gtwm.co.uk>
2013-03-02 21:51:57 +0000

    don't action header clicks debug

--- gtpb_clientside/resources/simple/simple.js
+++ gtpb_clientside/resources/simple/simple.js
@@ -223,8 +223,7 @@ function tileLoaded(tile) {
 
 function reportRowClicks() {
 	$(".reportData tr").click(function(event) {
-				var thead = $(event.target).closest("div");
-				console.log(thead[0]);
+				console.log(event.target.nodeName);
 				if ($(event.target).closest("thead").size() > 0) {
 					return;
 				}
