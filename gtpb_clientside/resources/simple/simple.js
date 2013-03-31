3936452de6436303257698af6ccbf6908f4dc558
Oliver Kohll <oliver@gtwm.co.uk>
2013-03-31 14:43:53 +0100

    Reload agenda when back home

--- gtpb_clientside/resources/simple/simple.js
+++ gtpb_clientside/resources/simple/simple.js
@@ -595,7 +595,6 @@ function backHome() {
 	$("#report_selection").addClass("notfocus");
 	$("#agenda").removeClass("notfocus");
 	var internalTileName = $(".tile.calendar").attr("data-internaltilename");
-	alert("internalTileName " + internalTileName);
 	$(".tile.calendar").find(".content").load("AppController.servlet", {
 		"return": "s/tiles/calendar",
 		set_tile: internalTileName
