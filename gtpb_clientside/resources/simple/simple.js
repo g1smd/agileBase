02c5d3172a5a9d299c68eb1510323d046b9b92c6
Oliver Kohll <oliver@gtwm.co.uk>
2013-03-31 13:19:09 +0100

    Add event buttons css

--- gtpb_clientside/resources/simple/simple.js
+++ gtpb_clientside/resources/simple/simple.js
@@ -207,10 +207,11 @@ function loadCalendar(calendarElement) {
 				},
 				dayClick: function(date, allDay, jsEvent, view) {
 					var dayElement = $(this);
+					var colour = dayElement.closest(".tile").attr("data-colour");
 					var addEventElement = dayElement.append("<div class='addEvents' class='transition notfocus'></div>");
 					var addEventElement = dayElement.find(".addEvents");
 					$("#report_selection_header span").each(function() {
-						addEventElement.append("<span class='addEvent'>add " + $(this).attr("data-singulartablename") + "</span>");
+						addEventElement.append("<span class='addEvent " + colour + "_bg'">add " + $(this).attr("data-singulartablename") + "</span>");
 					});
 					addEventElement.removeClass("notfocus");
 				},
