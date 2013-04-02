ea0f981428069a1754b5219e35c0de3a73f8d3c9
Oliver Kohll <oliver@gtwm.co.uk>
2013-04-02 11:45:03 +0100

    inlineComplete debug

--- gtpb_clientside/resources/tabs/tabs.js
+++ gtpb_clientside/resources/tabs/tabs.js
@@ -260,7 +260,7 @@ function fSetupTagRemove(tag) {
 
 function fTags() {
 	if (typeof $("<input></input>").inlineComplete != "function") {
-		console.log("inlineComplete not a function");
+		console.log("inlineComplete not defined");
 		// For some reason, the first time fTags is called, the inlineComplete plugin isn't present/initialised
 		return;
 	}
