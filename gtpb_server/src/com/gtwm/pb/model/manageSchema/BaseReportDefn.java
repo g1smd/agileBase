9a468759b79b962ea4905625e56f00abeb302e64
Oliver Kohll <oliver@gtwm.co.uk>
2013-05-06 14:59:31 +0100

    Cal. text colour

--- gtpb_server/src/com/gtwm/pb/model/manageSchema/BaseReportDefn.java
+++ gtpb_server/src/com/gtwm/pb/model/manageSchema/BaseReportDefn.java
@@ -98,7 +98,7 @@ public abstract class BaseReportDefn implements BaseReportInfo {
 		int hash3 = Math.abs(temp.reverse().toString().hashCode());
 		int hue = hash1 % 360;
 		int saturation = (hash2 % 40) + 60;
-		int lightness = (hash3 % 45) + 55;
+		int lightness = (hash3 % 20) + 80;
 		return "hsl(" + hue + "," + saturation + "%," + lightness + "%)";
 	}
 
